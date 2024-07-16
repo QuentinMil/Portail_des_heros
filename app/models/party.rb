e5dc43ead336d1b4ff333f48a5d2dd7dbad7dcf2
PAxNova <votre.email@example.com>
2024-07-16 12:21:21 +0200

    MAJ Party model

--- app/models/party.rb
+++ app/models/party.rb
@@ -1,4 +1,8 @@
 class Party < ApplicationRecord
-  belongs_to :universe
   belongs_to :user
+  belongs_to :universe
+  has_many :messages
+  has_and_belongs_to_many :characters, join_table: :party_characters
+  
+  validates :name, presence: true
 end
