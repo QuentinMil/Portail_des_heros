c3ef445bace1099ee6cde229779e4dceaea00f67
PAxNova <votre.email@example.com>
2024-07-16 12:14:26 +0200

    MAJ Character Model

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,9 @@
 class Character < ApplicationRecord
   belongs_to :user
   belongs_to :universe
+
+  has_many :notes
+  has_and_belongs_to_many :parties, join_table: :party_characters
+
+  validates :name, :strength, :dexterity, :intelligence, :constitution, :wisdom, :charisma, :available_status, presence: true
 end
