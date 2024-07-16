a87b428bf6b8d4bb2be1a391d7ff3c3cd41e1450
PAxNova <votre.email@example.com>
2024-07-16 12:49:59 +0200

    erreur de syntaxe sur Univers Model

--- app/models/universe.rb
+++ app/models/universe.rb
@@ -1,7 +1,6 @@
 class Universe < ApplicationRecord
-    
-    has_many: :characters
-    has_many: :parties
+    has_many :characters
+    has_many :parties
 
     validates :name, presence: true
     validates :description, presence: true
