3e02f5d87111d42744d071eacf025be16c564b7e
PAxNova <votre.email@example.com>
2024-07-17 16:15:42 +0200

    ajout du controller Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
   devise_for :users
   root to: "pages#home"
 
-  
+  # Les routes pour notre lexique (Post)
+  resources :posts, only: [:index, :show]
+
 end
