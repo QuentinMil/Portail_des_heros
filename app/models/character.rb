3cc44757233bed55a39bd5402f4f46b3c32cb2ea
PAxNova <votre.email@example.com>
2024-07-18 13:43:15 +0200

    method pour autoupdate du completion rate

--- app/models/character.rb
+++ app/models/character.rb
@@ -7,4 +7,15 @@ class Character < ApplicationRecord
   has_many :notes
   has_many :party_characters
   has_many :parties, through: :party_characters
+
+  def update_completion_rate
+    rate = 0
+    rate += 1 if self.universe.present?
+    rate += 1 if self.race.present?
+    rate += 1 if self.univers_class.present?
+    rate += 1 if self.strength.present?
+    rate += 1 if self.dexterity.present?
+    rate += 1 if self.intelligence.present?
+    self.update(completion_rate: rate)
+  end
 end
