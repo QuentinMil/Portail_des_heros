2cd073b86a319a26f56e6de200911247e67140a7
PAxNova <votre.email@example.com>
2024-07-20 19:02:36 +0200

    il faut effacer les notes avant les characters

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,9 +4,18 @@ class Character < ApplicationRecord
   belongs_to :race, optional: true
   belongs_to :univers_class, optional: true
 
-  has_many :notes
-  has_many :party_characters
+  has_many :notes, dependent: :destroy
+  has_many :party_characters, dependent: :destroy
   has_many :parties, through: :party_characters
 
   has_one_attached :photo
+
+  # il faut effacer les notes de notre character avant de le supprimer. Autrement il y aura une erreur de clé étrangère
+  before_destroy :destroy_notes
+  
+  private 
+
+  def destroy_notes
+    notes.destroy_all
+  end
 end
