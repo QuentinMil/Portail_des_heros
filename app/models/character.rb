42813d4952051d7953c085d57f070d2776ac146d
PAxNova <votre.email@example.com>
2024-07-20 17:54:54 +0200

    clean du model character

--- app/models/character.rb
+++ app/models/character.rb
@@ -9,18 +9,4 @@ class Character < ApplicationRecord
   has_many :parties, through: :party_characters
 
   has_one_attached :photo
-
-  def update_completion_rate
-    rate = 0
-    rate += 1 if self.universe.present?
-    rate += 1 if self.race.present?
-    rate += 1 if self.univers_class.present?
-    rate += 1 if self.strength.present?
-    rate += 1 if self.dexterity.present?
-    rate += 1 if self.intelligence.present?
-    rate += 1 if self.constitution.present?
-    rate += 1 if self.wisdom.present?
-    rate += 1 if self.charisma.present?
-    self.update(completion_rate: rate)
-  end
 end
