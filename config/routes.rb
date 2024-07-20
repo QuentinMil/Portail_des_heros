dfd09eb8930d2ff4cee9515193dad5396a5171e9
PAxNova <votre.email@example.com>
2024-07-20 18:26:25 +0200

    création d'une route pour tous les perso

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,11 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   # A FAIRE : Limiter le nombre de routes pour des questions de sécurité
+  resources :characters, path: 'mes_personnages' do
+
   # création d'une route pour afficher tous les personnages créés sur le serveur
-  resources :characters as: 'mes_personnages' do
-    collection do
-      get 'all', to: 'characters#all_characters', as: 'tous_les_personnages'
-    end
-  end
+  get '/tous_les_personnages', to: 'characters#all_characters', as: 'tous_les_personnages'
+
 
   # Les routes pour notre lexique (Post)
   resources :posts, only: [:index, :show]
