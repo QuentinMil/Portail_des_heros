ff05912438b3d6025b81a850afa3264e4c760f38
PAxNova <votre.email@example.com>
2024-07-22 17:17:07 +0200

    ajouter un espace pour afficher la backstory

--- app/models/character.rb
+++ app/models/character.rb
@@ -16,18 +16,27 @@ class Character < ApplicationRecord
   def generate_backstory
     prompt_template = YAML.load_file(Rails.root.join('config/prompts.yml'))['generate_backstory']['template']
     prompt = prompt_template % { name: name, race: race.name, univers_class: univers_class.name, universe: universe.name }
-
+  
     client = OpenAI::Client.new
-    response = client.chat(
-      parameters: {
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: prompt }],
-        temperature: 0.7
-      }
-    )
+    response = nil
 
+    begin
+      response = client.chat(
+        parameters: {
+          model: "gpt-3.5-turbo",
+          messages: [{ role: "user", content: prompt }],
+          temperature: 0.7
+        }
+      )
+    rescue Faraday::TooManyRequestsError => e
+      puts "Rate limit exceeded. Retrying after delay..."
+      sleep(60) # Attendez 60 secondes avant de réessayer
+      retry
+    end
+  
     self.update(backstory: response.dig("choices", 0, "message", "content"))
   end
+  
   
   private 
 
