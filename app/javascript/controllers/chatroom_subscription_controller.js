c3b8d0879bbff99a13ebd1ecf9a710ae935fad39
PAxNova <votre.email@example.com>
2024-07-22 13:39:57 +0200

    MAJ du stimulus pour la chatroom

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -1,7 +1,30 @@
+// app/javascript/controllers/chatroom_subscription_controller.js
 import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable"
 
-// Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
+  static values = { partyId: Number }
+  static targets = ["messages"]
+
   connect() {
+    this.subscription = createConsumer().subscriptions.create(
+      { channel: "PartyChannel", id: this.partyIdValue },
+      { received: data => this.#insertMessageAndScrollDown(data) }
+    )
+    console.log(`Subscribed to the party with the id ${this.partyIdValue}.`)
+  }
+
+  disconnect() {
+    console.log("Unsubscribed from the party")
+    this.subscription.unsubscribe()
+  }
+
+  #insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+
+  resetForm(event) {
+    event.target.reset()
   }
 }
