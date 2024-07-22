94968c0ce42d7a65ff78286faa1771552916a2ba
PAxNova <votre.email@example.com>
2024-07-22 13:37:07 +0200

    creation d'un message et push avec ActionCable

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,28 @@
+# app/controllers/messages_controller.rb
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+    @party = Party.find(params[:party_id])
+    @message = @party.messages.build(message_params)
+    @message.user = current_user
+    if @message.save
+      # Diffuser le message via ActionCable
+      ActionCable.server.broadcast "party_#{@party.id}_channel",
+                                   message: render_message(@message)
+      head :ok
+    else
+      redirect_to party_path(@party), alert: 'Votre message ne peut pas être vide.'
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+
+  def render_message(message)
+    ApplicationController.renderer.render(partial: 'messages/message', locals: { message: message })
   end
 end
