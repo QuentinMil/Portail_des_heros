d642cd3c004454c810c77602edc89daa00c55f24
PAxNova <votre.email@example.com>
2024-07-23 22:29:57 +0200

    add to application

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module PortailDesHeros
   class Application < Rails::Application
     config.action_controller.raise_on_missing_callback_actions = false if Rails.version >= "7.1.0"
+    config.active_job.queue_adapter = :sidekiq
     config.generators do |generate|
       generate.assets false
       generate.helper false
