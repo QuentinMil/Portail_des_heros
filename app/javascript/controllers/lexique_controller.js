67c2b70788bd581232f1fdd4c62b6f6dab9c4f26
PAxNova <votre.email@example.com>
2024-07-23 15:44:40 +0200

    MAJ du stimulus

--- app/javascript/controllers/lexique_controller.js
+++ app/javascript/controllers/lexique_controller.js
@@ -1,4 +1,5 @@
-import { Controller } from "stimulus";
+// app/javascript/controllers/lexique_controller.js
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["content"];
@@ -11,14 +12,11 @@ export default class extends Controller {
     event.preventDefault();
     const url = event.currentTarget.getAttribute("href");
 
-    fetch(url, {
-      headers: {
-        'Accept': 'text/vnd.turbo-stream.html'
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      this.contentTarget.innerHTML = `<h2>${data.title}</h2><p>${data.content}</p>`;
-    });
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.contentTarget.innerHTML = `<h2>${data.title}</h2><p>${data.content}</p>`;
+      })
+      .catch(error => console.error("Error fetching the JSON:", error));
   }
 }
