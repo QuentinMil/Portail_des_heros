45018aa3c32c40fe5344de6858b124ea4bf3d60d
PAxNova <votre.email@example.com>
2024-07-23 16:03:26 +0200

    maj du post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,15 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+
+    if params[:query].present?
+      @posts = @posts.where('title ILIKE ?', "%#{params[:query]}%")
+    end
+
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: 'posts/list', locals: { posts: @posts } }
+    end
   end
 
   def show
--- app/javascript/controllers/lexique_controller.js
+++ app/javascript/controllers/lexique_controller.js
@@ -1,8 +1,8 @@
 // app/javascript/controllers/lexique_controller.js
-import { Controller } from "@hotwired/stimulus";
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["content"];
+  static targets = ["form", "list", "searchInput", "content"]
 
   connect() {
     console.log("Lexique controller connected");
@@ -16,7 +16,15 @@ export default class extends Controller {
       .then(response => response.json())
       .then(data => {
         this.contentTarget.innerHTML = `<h2>${data.title}</h2><p>${data.content}</p>`;
-      })
-      .catch(error => console.error("Error fetching the JSON:", error));
+      });
+  }
+
+  update() {
+    const url = `${this.formTarget.action}?query=${this.searchInputTarget.value}`
+    fetch(url, { headers: { 'Accept': 'text/plain' } })
+      .then(response => response.text())
+      .then((data) => {
+        this.listTarget.outerHTML = data;
+      });
   }
 }
