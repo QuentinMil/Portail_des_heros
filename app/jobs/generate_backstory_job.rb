becc56257ab59db06e2a684ae95042db99f14f18
PAxNova <votre.email@example.com>
2024-07-23 23:06:44 +0200

    add variables to the backstory job

--- app/jobs/generate_backstory_job.rb
+++ app/jobs/generate_backstory_job.rb
@@ -1,12 +1,22 @@
-# app/jobs/generate_backstory_job.rb
 class GenerateBackstoryJob < ApplicationJob
   queue_as :default
 
   def perform(character_id)
     character = Character.find(character_id)
     prompt_template = YAML.load_file(Rails.root.join('config/prompts.yml'))['generate_backstory']['template']
-    prompt = prompt_template % { name: character.name, race: character.race.name, univers_class: character.univers_class.name, universe: character.universe.name }
-  
+    prompt = prompt_template % {
+      name: character.name,
+      race: character.race.name,
+      univers_class: character.univers_class.name,
+      universe: character.universe.name,
+      strength: character.strength,
+      dexterity: character.dexterity,
+      intelligence: character.intelligence,
+      constitution: character.constitution,
+      wisdom: character.wisdom,
+      charisma: character.charisma
+    }
+
     client = OpenAI::Client.new
     response = nil
 
@@ -24,6 +34,7 @@ class GenerateBackstoryJob < ApplicationJob
       retry
     end
 
-    character.update(backstory: response.dig("choices", 0, "message", "content"))
+    backstory_content = response.dig("choices", 0, "message", "content")
+    character.update(backstory: backstory_content) if backstory_content.present?
   end
 end
