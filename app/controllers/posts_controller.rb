dd4c0b43283fbf3e8a0ed51cc306b08c901b045e
PAxNova <votre.email@example.com>
2024-07-23 16:49:37 +0200

    ajout d'une div

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,14 @@
+# app/controllers/posts_controller.rb
 class PostsController < ApplicationController
   def index
     if params[:query].present?
-      @posts = Post.search_by_title_and_content(params[:query])
+      @posts = Post.where("title ILIKE ?", "%#{params[:query]}%")
     else
       @posts = Post.all
     end
 
     respond_to do |format|
-      format.html
-      format.json { render json: @posts }
+      format.html # Follow regular flow of Rails
       format.text { render partial: 'posts/list', locals: { posts: @posts } }
     end
   end
--- app/javascript/controllers/lexique_controller.js
+++ app/javascript/controllers/lexique_controller.js
@@ -10,20 +10,34 @@ export default class extends Controller {
   load(event) {
     event.preventDefault();
     const url = event.currentTarget.getAttribute("href");
+    console.log(`Fetching content from URL: ${url}`);
 
     fetch(url)
       .then(response => response.json())
       .then(data => {
+        console.log("Received data:", data);
         this.contentTarget.innerHTML = `<h2>${data.title}</h2><p>${data.content}</p>`;
+      })
+      .catch(error => {
+        console.error("Error fetching content:", error);
       });
   }
 
   update() {
     const url = `${this.formTarget.action}?query=${this.searchInputTarget.value}`;
+    console.log(`Updating list with URL: ${url}`);
+
     fetch(url, { headers: { 'Accept': 'text/plain' } })
-      .then(response => response.text())
-      .then((data) => {
+      .then(response => {
+        console.log("Response received:", response);
+        return response.text();
+      })
+      .then(data => {
+        console.log("Data received for list update:", data);
         this.listTarget.outerHTML = data;
+      })
+      .catch(error => {
+        console.error("Error updating list:", error);
       });
   }
 }
