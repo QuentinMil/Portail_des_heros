5ae3bf02690b40659d5136dfc57db7bcbc55d434
PAxNova <votre.email@example.com>
2024-07-23 16:28:40 +0200

    modif de la class post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,12 @@
 class Post < ApplicationRecord
   validates :title, :content, presence: true
-
   has_one_attached :photo
+
+  include PgSearch::Model
+
+  pg_search_scope :search_by_title_and_content, 
+                  against: [:title, :content],
+                  using: {
+                    tsearch: { prefix: true } # Le préfixe permet des correspondances partielles
+                  }
 end
