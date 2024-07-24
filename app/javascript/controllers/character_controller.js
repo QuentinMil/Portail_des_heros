484d6cf9d61ce3511b9aa64d70635aef8fa65786
PAxNova <votre.email@example.com>
2024-07-24 12:52:12 +0200

    RAZ du character_controller

--- app/javascript/controllers/character_controller.js
+++ app/javascript/controllers/character_controller.js
@@ -1,32 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
-import consumer from "../channels/consumer"
 
 // Connects to data-controller="character"
 export default class extends Controller {
-  static targets = ["input", "click", "backstory", "photo"]
+  static targets = [ "input", "click" ]
 
   connect() {
-    // Debug message when the controller connects
-    console.log("Character controller connected");
-
-    // Abonnement au canal ActionCable
-    console.log("Connecting to ActionCable channel: CharacterChannel");
-    this.channel = consumer.subscriptions.create("CharacterChannel", {
-      received: (data) => {
-        console.log("Received data from ActionCable:", data);
-        if (data.character_id === parseInt(this.data.get("characterId"))) {
-          this.updateCharacter(data)
-        }
-      },
-      connected: () => {
-        console.log("Successfully connected to CharacterChannel");
-      },
-      disconnected: () => {
-        console.log("Disconnected from CharacterChannel");
-      }
-    });
-
-    console.log("Character controller fully set up.");
+    // console.log(this.inputTargets)
   }
 
   disabled(event) {
@@ -49,14 +28,12 @@ export default class extends Controller {
     })
   }
 
-
   random(event) {
-    console.log("Random event triggered");
-    // Je construis un array avec mes valeurs [8, 10, 12, 13, 14, 15]
+    // je construit un array avec mes valeurs [8, 10, 12, 13, 14, 15]
     const valeurs = [8, 10, 12, 13, 14, 15]
-    // Je shuffle mon array
+    // je shuffle mon array
     const random = this.#shuffleArray(valeurs)
-    // J'itere sur mes inputs et je leur assigne une valeur
+    // j'itere sur mes inputs et je leur assigne une valeur
     this.inputTargets.forEach((input) => {
       input.value = random.pop()
     })
@@ -64,18 +41,10 @@ export default class extends Controller {
   }
 
   #shuffleArray(array) {
-    console.log("Shuffling array");
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]]; // Échange des éléments
+      [array[i], array[j]] = [array[j], array[i]]; // échange des éléments
     }
     return array;
-  }
-
-  updateCharacter(data) {
-    console.log("Updating character with data:", data);
-    alert(data.message)
-    this.backstoryTarget.innerHTML = data.backstory
-    this.photoTarget.src = data.photo_url
   }
 }
