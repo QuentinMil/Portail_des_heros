6e3d4c00455829755dd1cf6819192a76dcfa5faa
PAxNova <votre.email@example.com>
2024-07-24 16:22:07 +0200

    double abonnement réglé

--- app/javascript/controllers/backstory_controller.js
+++ app/javascript/controllers/backstory_controller.js
@@ -5,32 +5,44 @@ import consumer from "../channels/consumer"
 // Connects to data-controller="backstory"
 export default class extends Controller {
   static targets = ["backstory", "photo"]
+  static values = { characterId: Number }
 
   connect() {
-    console.log("Backstory controller connected");
+    console.log("Backstory controller connected for character ID:", this.characterIdValue);
 
-    // Abonnement au canal ActionCable
-    console.log("Connecting to ActionCable channel: CharacterChannel");
-    this.channel = consumer.subscriptions.create("CharacterChannel", {
-      received: (data) => {
-        // console.log("Received data from ActionCable:", data);
-        if (data.character_id === parseInt(this.data.get("characterId"))) {
-          this.updateCharacter(data)
+    if (!this.channel) {
+      // Abonnement au canal ActionCable
+      console.log("Connecting to ActionCable channel: CharacterChannel");
+      this.channel = consumer.subscriptions.create(
+        { channel: "CharacterChannel", character_id: this.characterIdValue }, {
+        received: (data) => {
+          console.log("Received data from ActionCable:", data);
+          if (data.character_id === this.characterIdValue) {
+            this.updateCharacter(data);
+          }
+        },
+        connected: () => {
+          console.log("Successfully connected to CharacterChannel for character ID:", this.characterIdValue);
+        },
+        disconnected: () => {
+          console.log("Disconnected from CharacterChannel for character ID:", this.characterIdValue);
         }
-      },
-      connected: () => {
-        console.log("Successfully connected to CharacterChannel");
-      },
-      disconnected: () => {
-        console.log("Disconnected from CharacterChannel");
-      }
-    });
+      });
 
-    console.log("Backstory controller fully set up.");
+      console.log("Backstory controller fully set up for character ID:", this.characterIdValue);
+    }
+  }
+
+  disconnect() {
+    if (this.channel) {
+      this.channel.unsubscribe();
+      this.channel = null;
+      console.log("Unsubscribed from CharacterChannel for character ID:", this.characterIdValue);
+    }
   }
 
   updateCharacter(data) {
-    // console.log("Updating character with data:", data);
+    console.log("Updating character with data:", data);
     alert(data.message);
 
     // Met à jour la photo
